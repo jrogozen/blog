b79d799efe6ff85274ac2384c874e441651c6f7d
Jon Rogozen <jon.rogozen@gmail.com>
2015-01-11 18:28:54 -0800

    show blurb on category page w/ link to post

--- spec/features/categories/category_index_spec.rb
+++ spec/features/categories/category_index_spec.rb
@@ -4,8 +4,8 @@ feature "Render Categories page", js: true do
   before do
     Category.create!(name: 'Books', id: 1)
     Category.create!(name: 'Ruby', id: 2)
-    Post.create!(name: 'My Favorite Novel', category_id: 1, id: 1)
-    Post.create!(name: 'My Favorite Script', category_id: 2, id: 2)
+    Post.create!(name: 'My Favorite Novel', category_id: 1, id: 1, blurb: "Novel blurb")
+    Post.create!(name: 'My Favorite Script', category_id: 2, id: 2, blurb: "Script blurb")
   end
 
   scenario "displays categories" do
@@ -14,11 +14,11 @@ feature "Render Categories page", js: true do
     expect(page).to have_content("Books")
     expect(page).to have_content("Ruby")
   end
-
-  scenario "displays posts for each category" do
+  
+  scenario "displays blurbs for each post" do
     visit "/"
     click_on "Categories"
-    expect(page).to have_content("My Favorite Novel")
-    expect(page).to have_content("My Favorite Script")
+    expect(page).to have_content("Script blurb")
+    expect(page).to have_content("Novel blurb")
   end
 end
